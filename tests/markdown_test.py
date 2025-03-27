95d042ef267afa3dd6f31bd293c16e71af4cefa6
Thomas Weigel <weigel-thomas@outlook.de>
2025-03-27 09:43:41 +0100

    add Licences of html2md binaries

--- tests/markdown_test.py
+++ tests/markdown_test.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Deutsche Telekom AG (opensource@telekom.de)
+#
+# SPDX-License-Identifier: Apache-2.0
+
 import tempfile
 from argparse import Namespace
 from pathlib import Path
