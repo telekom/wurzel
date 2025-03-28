ddd111941f03f2c89c9101c279d88b1f59fe82e9
Thomas Weigel <weigel-thomas@outlook.de>
2025-03-28 11:42:39 +0100

    reuse lint

--- tests/drop_duplication_test.py
+++ tests/drop_duplication_test.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightmd: 2025 Deutsche Telekom AG (opensource@telekom.de)
+# SPDX-FileCopyrightText: 2025 Deutsche Telekom AG (opensource@telekom.de)
 #
 # SPDX-License-Identifier: Apache-2.0
 
