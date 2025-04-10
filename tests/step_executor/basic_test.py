f22d575a53f41f738c3522edf077c9b928ff8699
tweigel-dev <56434333+tweigel-dev@users.noreply.github.com>
2025-04-10 12:58:35 +0200

    fix: add tlsh dep to qdrant (#31)
    * add tlsh dep to qdrant
    
    * chore: rename test-classes
    
    * chore: typo in test file
    
    * feat: implement common bools for optional dependencies
    
    * fix: add text_tlsh_hash to Qdrant[...]Result
    
    * feat: use tlsh optionally in Qdrant Steps
    
    * chore: update & simplify tests
    
    * fix: test-list
    
    * move tlsh dep to seperate group
    
    * chore: rename get_hashes func
    
    ---------
    
    Co-authored-by: Max Erren <maximilian.erren@telekom.de>

--- tests/step_executor/basic_test.py
+++ tests/step_executor/basic_test.py
@@ -34,26 +34,22 @@ def test_file_in_memory_out(tmp_path):
     assert res[0][1].time_to_save == 0
 
 
-class TestAStep(TypedStep[NoSettings, None, MarkdownDataContract]):
+class TstAStep(TypedStep[NoSettings, None, MarkdownDataContract]):
     def run(self, inputs: None) -> MarkdownDataContract:
         return MarkdownDataContract(md="A", keywords="A", url="A")
 
 
-class TestA2Step(TypedStep[NoSettings, None, MarkdownDataContract]):
+class TstA2Step(TypedStep[NoSettings, None, MarkdownDataContract]):
     def run(self, inputs: None) -> MarkdownDataContract:
         return MarkdownDataContract(md="A2", keywords="A2", url="A2")
 
 
-class TestBStep(
-    TypedStep[NoSettings, MarkdownDataContract, list[MarkdownDataContract]]
-):
+class TstBStep(TypedStep[NoSettings, MarkdownDataContract, list[MarkdownDataContract]]):
     def run(self, inputs: MarkdownDataContract) -> list[MarkdownDataContract]:
         return [inputs, inputs]
 
 
-class TestCStep(
-    TypedStep[NoSettings, list[MarkdownDataContract], MarkdownDataContract]
-):
+class TstCStep(TypedStep[NoSettings, list[MarkdownDataContract], MarkdownDataContract]):
     def run(self, inputs: list[MarkdownDataContract]) -> MarkdownDataContract:
         return inputs[0]
 
@@ -63,9 +59,9 @@ def test_chain(tmp_path):
     out_b = tmp_path / "out_b"
     out_c = tmp_path / "out_c"
     with BaseStepExecutor() as ex:
-        a = ex.execute_step(TestAStep, None, out_a)
-        b = ex.execute_step(TestBStep, (out_a,), out_b)
-        c = ex.execute_step(TestCStep, (out_b,), out_c)
+        a = ex.execute_step(TstAStep, None, out_a)
+        b = ex.execute_step(TstBStep, (out_a,), out_b)
+        c = ex.execute_step(TstCStep, (out_b,), out_c)
     for i in [a, b, c]:
         assert len(i) == 1
     for res, _rep in [a[0], c[0]]:
@@ -83,13 +79,13 @@ def test_2_to_1(tmp_path):
     out_as = [tmp_path / "out_a1", tmp_path / "out_a2"]
     out_b = tmp_path / "out_b"
     with BaseStepExecutor() as ex:
-        ex.execute_step(TestAStep, None, output_dir=out_as[0])
-        ex.execute_step(TestA2Step, None, output_dir=out_as[1])
-        b = ex.execute_step(TestBStep, out_as, out_b)
+        ex.execute_step(TstAStep, None, output_dir=out_as[0])
+        ex.execute_step(TstA2Step, None, output_dir=out_as[1])
+        b = ex.execute_step(TstBStep, out_as, out_b)
     assert len(b) == 2
     assert len(list(out_b.glob("*"))) == 2
     for p in out_b.glob("*"):
-        assert p.name in ["TestA-TestB.json", "TestA2-TestB.json"]
+        assert p.name in ["TstA-TstB.json", "TstA2-TstB.json"]
 
 
 def test_2_to_2_to_1(tmp_path):
@@ -102,21 +98,21 @@ def test_2_to_2_to_1(tmp_path):
     out_b1 = tmp_path / "out_b1"
     final = tmp_path / "final"
     with BaseStepExecutor() as ex:
-        ex.execute_step(TestAStep, None, output_dir=out_as[0])
-        ex.execute_step(TestA2Step, None, output_dir=out_as[1])
+        ex.execute_step(TstAStep, None, output_dir=out_as[0])
+        ex.execute_step(TstA2Step, None, output_dir=out_as[1])
         for out in out_as:
-            ex.execute_step(TestBStep, (out,), out_b1)
-        c = ex.execute_step(TestCStep, (out_b1,), final)
+            ex.execute_step(TstBStep, (out,), out_b1)
+        c = ex.execute_step(TstCStep, (out_b1,), final)
     assert len(c) == 2
     assert len(list(final.glob("*"))) == 2
 
-    assert list(out_as[0].glob("*"))[0].name == "TestA.json"
-    assert list(out_as[1].glob("*"))[0].name == "TestA2.json"
+    assert list(out_as[0].glob("*"))[0].name == "TstA.json"
+    assert list(out_as[1].glob("*"))[0].name == "TstA2.json"
     for p in out_b1.glob("*"):
-        assert p.name in ["TestA-TestB.json", "TestA2-TestB.json"]
+        assert p.name in ["TstA-TstB.json", "TstA2-TstB.json"]
 
     for p in final.glob("*"):
-        assert p.name in ["TestA-TestB-TestC.json", "TestA2-TestB-TestC.json"]
+        assert p.name in ["TstA-TstB-TstC.json", "TstA2-TstB-TstC.json"]
 
 
 def test_2_to_1_to_1(tmp_path):
@@ -129,11 +125,11 @@ def test_2_to_1_to_1(tmp_path):
     out_b1 = tmp_path / "out_b1"
     final = tmp_path / "final"
     with BaseStepExecutor() as ex:
-        ex.execute_step(TestAStep, None, output_dir=out_as[0])
-        ex.execute_step(TestA2Step, None, output_dir=out_as[1])
-        _ = ex.execute_step(TestBStep, out_as, out_b1)
-        c = ex.execute_step(TestCStep, (out_b1,), final)
+        ex.execute_step(TstAStep, None, output_dir=out_as[0])
+        ex.execute_step(TstA2Step, None, output_dir=out_as[1])
+        _ = ex.execute_step(TstBStep, out_as, out_b1)
+        c = ex.execute_step(TstCStep, (out_b1,), final)
     assert len(c) == 2
     assert len(list(final.glob("*"))) == 2
     for p in final.glob("*"):
-        assert p.name in ["TestA-TestB-TestC.json", "TestA2-TestB-TestC.json"]
+        assert p.name in ["TstA-TstB-TstC.json", "TstA2-TstB-TstC.json"]
