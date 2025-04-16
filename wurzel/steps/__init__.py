8fc46e4d9429eb6395ee31135ade019083641b01
Thomas Weigel <weigel-thomas@outlook.de>
2025-04-16 14:48:58 +0200

    qdrant conditional in init

--- wurzel/steps/__init__.py
+++ wurzel/steps/__init__.py
@@ -2,16 +2,17 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from wurzel.utils import HAS_QDRANT as _HAS_QDRANT
 
 from ..step import TypedStep
 from .embedding import EmbeddingStep
 from .manual_markdown import ManualMarkdownStep
-from .qdrant import QdrantConnectorMultiVectorStep, QdrantConnectorStep
 
-__all__ = [
-    "TypedStep",
-    "ManualMarkdownStep",
-    "EmbeddingStep",
-    "QdrantConnectorMultiVectorStep",
-    "QdrantConnectorStep",
-]
+__all__ = ["TypedStep", "ManualMarkdownStep", "EmbeddingStep"]
+if _HAS_QDRANT:
+    from .qdrant import QdrantConnectorMultiVectorStep, QdrantConnectorStep
+
+    __all__ += [
+        "QdrantConnectorMultiVectorStep",
+        "QdrantConnectorStep",
+    ]
