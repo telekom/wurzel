ddfb84cbee4868ca23bcc406e9e2ca77ee14c640
Thomas Weigel <weigel-thomas@outlook.de>
2025-04-17 15:48:28 +0200

    validate env settings https://github.com/telekom/wurzel/issues/39

--- wurzel/cli/cmd_generate.py
+++ wurzel/cli/cmd_generate.py
@@ -6,7 +6,8 @@ from pathlib import Path
 
 from wurzel.adapters.dvc_adapter import Backend
 from wurzel.step.typed_step import TypedStep
-
+from wurzel.step_executor.base_executor import BaseStepExecutor
+from wurzel.utils import create_model
 
 def main(step: TypedStep, data_dir: Path, backend: Backend) -> str:
     """
@@ -14,4 +15,10 @@ def main(step: TypedStep, data_dir: Path, backend: Backend) -> str:
     """
 
     adapter: Backend = backend(data_dir)
+    # validate the envs of the steps
+    create_model(
+        list(step.traverse()),
+        allow_extra_fields=BaseStepExecutor.is_allow_extra_settings()
+        )()
+
     return adapter.generate_yaml(step)
