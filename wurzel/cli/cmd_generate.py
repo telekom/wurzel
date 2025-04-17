11fe4a90658343968bc90dbe9cbb80d45208db02
Thomas Weigel <weigel-thomas@outlook.de>
2025-04-17 16:02:16 +0200

    wurzel lint

--- wurzel/cli/cmd_generate.py
+++ wurzel/cli/cmd_generate.py
@@ -9,6 +9,7 @@ from wurzel.step.typed_step import TypedStep
 from wurzel.step_executor.base_executor import BaseStepExecutor
 from wurzel.utils import create_model
 
+
 def main(step: TypedStep, data_dir: Path, backend: Backend) -> str:
     """
     Generates the yaml for the given backend
@@ -18,7 +19,7 @@ def main(step: TypedStep, data_dir: Path, backend: Backend) -> str:
     # validate the envs of the steps
     create_model(
         list(step.traverse()),
-        allow_extra_fields=BaseStepExecutor.is_allow_extra_settings()
-        )()
+        allow_extra_fields=BaseStepExecutor.is_allow_extra_settings(),
+    )()
 
     return adapter.generate_yaml(step)
