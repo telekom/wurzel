eb377ef5e76913ccbe5c2486a549e3801e6afd84
Max Erren <maximilian.erren@telekom.de>
2025-05-23 11:17:59 +0200

    fix: usage of __call__ in BaseStepExecutor

--- tests/step_executor/executor_prometheus_test.py
+++ tests/step_executor/executor_prometheus_test.py
@@ -3,8 +3,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+from wurzel.datacontract.common import MarkdownDataContract
+from wurzel.step.typed_step import TypedStep
 from wurzel.step_executor import PrometheusStepExecutor
 
+class DummyStep(TypedStep[None, None, MarkdownDataContract]):
+    def run(self, inpt: None) -> MarkdownDataContract:
+        return MarkdownDataContract(md="md", keywords="", url="")
 
 def test_create_metrics():
     executor = PrometheusStepExecutor()
@@ -20,3 +25,9 @@ def test_context_manager_singelton():
     with PrometheusStepExecutor() as exc:
         with PrometheusStepExecutor() as exc2:
             assert exc == exc2
+def test_setting_of_counters():
+    with PrometheusStepExecutor() as exc:
+        exc(DummyStep, None, None)
+        assert exc.counter_results.collect()[0].samples[0].value == 1.0
+        exc(DummyStep, None, None)
+        assert exc.counter_results.collect()[0].samples[0].value == 2.0
