a5ec221440b8f0e5ec69c2a3d6e65c76fb5d2e6a
Thomas Weigel <weigel-thomas@outlook.de>
2025-07-09 14:53:26 +0200

    lint

--- tests/step/backend_test.py
+++ tests/step/backend_test.py
@@ -1,11 +1,11 @@
-from wurzel.backend.backend_argo import ArgoBackend , ArgoBackendSettings, S3ArtifactTemplate
-from wurzel.backend.backend_dvc import DvcBackend
+# SPDX-FileCopyrightText: 2025 Deutsche Telekom AG (opensource@telekom.de)
+#
+# SPDX-License-Identifier: Apache-2.0
 
-
+from wurzel.backend.backend_argo import ArgoBackendSettings
 
 
 def test_argo_settings(env):
-
     settings = ArgoBackendSettings()
     assert settings.IMAGE == "ghcr.io/telekom/wurzel"
     env.set("ARGOWORKFLOWBACKEND__IMAGE", "test/image:latest")
