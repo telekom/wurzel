3a854b3154c03db644ce5d463ed44292c8bd58ed
sam-hey <40773225+sam-hey@users.noreply.github.com>
2025-10-06 20:37:53 +0200

    ref: middleware

--- wurzel/step_executor/middlewares/settings.py
+++ wurzel/step_executor/middlewares/settings.py
@@ -9,14 +9,6 @@ from pydantic import Field
 from wurzel.step.settings import Settings
 
 
-class PrometheusMiddlewareSettings(Settings):
-    """Configuration for Prometheus middleware."""
-
-    PROMETHEUS_GATEWAY: str = Field("localhost:9091", description="host:port of pushgateway")
-    PROMETHEUS_JOB: str = Field("default-job-name", description="jobname for the prometheus counter")
-    PROMETHEUS_DISABLE_CREATED_METRIC: bool = Field(True, description="disable *_created metrics")
-
-
 class MiddlewareSettings(Settings):
     """Global settings for middleware configuration."""
 
